cf55e94ee79d5af1194ed023dca0c0361a68a944
Dragoner <Dragonerfc@gmail.com>
2017-05-27 00:18:59 +0200

    Shaders Added

--- Engine/API/Interfaces/RSCommandBuffer.h
+++ Engine/API/Interfaces/RSCommandBuffer.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "../../RSIncludeVulkan.h"
 #include "../../Debug/DebugMacros.h"
-#include "../RS_API.h"
+//#include "../RS_API.h"
 
 
 namespace RedSt4R
@@ -21,8 +21,8 @@ namespace RedSt4R
 			virtual void SubmitToQueue(void* pQueue) = 0;
 			virtual void Reset() = 0;
 
-			virtual void rsCmdBindVertexBuffers(RSVertexBuffer* pVertexBuffers, uint32_t firstBinding, uint32_t bindingCount, int offsets) = 0;
-			virtual void rsCmdBindPipeline(RSGraphicsPipeline* pGraphicsPipeline, EPipelineBindPoint) = 0;
+			//virtual void rsCmdBindVertexBuffers(RSVertexBuffer* pVertexBuffers, uint32_t firstBinding, uint32_t bindingCount, int offsets) = 0;
+			//virtual void rsCmdBindPipeline(RSGraphicsPipeline* pGraphicsPipeline, EPipelineBindPoint) = 0;
 
 			static RSCommandBuffer* CreateCommandBuffer(float flags);
 		};
---
+++ Shaders/GLSL to SPIR-V.bat
@@ -0,0 +1,3 @@
+C:/VulkanSDK/1.0.46.0/Bin32/glslangValidator.exe -V simpleVS.vert
+C:/VulkanSDK/1.0.46.0/Bin32/glslangValidator.exe -V simplePS.frag
+pause
